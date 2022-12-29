245b2b27537aae3ddd5edbd3ac2d947400ba1830
Uchchash346 <mru416@gmail.com>
2022-12-29 10:16:46 +0600

    phone auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQj9gTigKyUlHZyz5RPoINoXu8lLsdCTk",
+    authDomain: "otp1-f6a77.firebaseapp.com",
+    projectId: "otp1-f6a77",
+    storageBucket: "otp1-f6a77.appspot.com",
+    messagingSenderId: "804183130339",
+    appId: "1:804183130339:web:826e552eb1f91ef163c5d6",
+    measurementId: "G-HWX6VESR0F"
+};
+
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+export { auth, firebase };
